b34f525271fec66a22fd31d794e23dd29b4867b2
Pramod Kumar Yadav <44602671+itzpkyadav@users.noreply.github.com>
2021-07-14 18:37:12 +0530

    PDF Viewer

---
+++ PdfViewActivity.java
@@ -0,0 +1,33 @@
+package com.yunolearning.learn.activity;
+
+import android.os.Bundle;
+
+import com.yunolearning.learn.R;
+
+public class PdfViewActivity extends BaseActivity {
+
+    @Override
+    int getLayout() {
+        return R.layout.activity_pdf_view;
+    }
+
+    @Override
+    void init(Bundle savedInstanceState) {
+/*
+        Uri Uri=
+
+        PDFView pdfView = findViewById(R.id.pdfView);
+
+        pdfView.fromUri(Uri);*/
+
+//        pdfView.fromAsset("richdadpoordad.pdf").load();
+
+//        downloadPdfFromInternet();
+    }
+
+    private void downloadPdfFromInternet() {
+
+    }
+
+
+}
