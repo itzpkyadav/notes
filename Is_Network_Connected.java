0834194049b16f52e47c202465f8ff7d07673954
Pramod Kumar Yadav <44602671+itzpkyadav@users.noreply.github.com>
2021-07-16 14:43:06 +0530

    Create Is_Network_Connected.java

---
+++ Is_Network_Connected.java
@@ -0,0 +1,11 @@
+ public static boolean isNetworkConnected(Context context) {
+        try {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
+            return (mNetworkInfo == null) ? false : true;
+
+        } catch (NullPointerException e) {
+            return false;
+
+        }
+    }
