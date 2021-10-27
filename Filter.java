0dbb5ea8808e907a90b9d8571ada1e0e56b8b27f
Pramod Kumar Yadav <44602671+itzpkyadav@users.noreply.github.com>
2021-10-27 19:35:42 +0530

    Create Filter.java

---
+++ Filter.java
@@ -0,0 +1,30 @@
+// first case   
+List<InstructorBatchList> myUniqueList = distinctList(instructorBatchLists, InstructorBatchList::getCourseId);
+
+// second case
+List<InstructorBatchList> uniqueObjects = instructorBatchLists.stream()
+                                        .collect(Collectors.groupingBy((p) -> p.getCourseId())) //expression
+                                        .values()
+                                        .stream()
+                                        .flatMap(e -> e.stream().limit(1))
+                                        .collect(Collectors.toList());
+
+  
+
+
+public static <T> List<T> distinctList(List<T> list, Function<? super T, ?>... keyExtractors) {
+
+        return list.stream().filter(distinctByKeys(keyExtractors)).collect(Collectors.toList());
+}
+
+private static <T> Predicate<T> distinctByKeys(Function<? super T, ?>... keyExtractors) {
+
+        final Map<List<?>, Boolean> seen = new ConcurrentHashMap<>();
+
+        return t -> {
+            final List<?> keys = Arrays.stream(keyExtractors).map(ke -> ke.apply(t)).collect(Collectors.toList());
+
+            return seen.putIfAbsent(keys, Boolean.TRUE) == null;
+
+        };
+}
